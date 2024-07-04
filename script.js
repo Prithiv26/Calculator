d3d6eac2f668914ef97e0f32871691e37dd59912
prithiv26 <itz.prithiv2006@gmail.com>
2024-07-04 13:59:10 +0530

    Updated bugs related to negative sign and added mobile support

--- script.js
+++ script.js
@@ -24,11 +24,18 @@ backspaceButton.addEventListener("click",backspaceHandler);
 function backspaceHandler(e)
 {
     let removedItem = textBox.value.at(-1);
-    textBox.value = textBox.value.slice(0,textBox.value.length -1);
+    if (removedItem === ' ')
+    {
+        removedItem = textBox.value.slice(textBox.value.length -3).trim();
+        textBox.value = textBox.value.slice(0,textBox.value.length -3);
+    }
+    else{
+        textBox.value = textBox.value.slice(0,textBox.value.length -1);
+    }
+    
     switch(removedItem)
     {
         case '+':
-        case '-':
         case 'x':
         case '/':
         case '%':
@@ -37,16 +44,31 @@ function backspaceHandler(e)
         case '.':
             decimalPointCheck = false;
             break;
-        case '+/-':
-            signCheck = false;
-        
-        
+        case '-':
+            if (operatorCheck === true && signCheck === true)
+            {
+                signCheck = false;
+            }
+            else if (operatorCheck === true)
+            {
+                operatorCheck = false;
+            }
+            if (textBox.value.includes('-'))
+            {
+                signCheck = true;
+            }
+            else
+            {
+                signCheck = false;
+            }
     }
 }
 
 let contents = document.querySelector(".contents");
 contents.addEventListener("mousedown",(e) => {e.target.classList.add("clickEffect")});
 contents.addEventListener("mouseup",(e) => {e.target.classList.remove("clickEffect")});
+contents.addEventListener("touchstart",(e) => {e.target.classList.add("clickEffect")});
+contents.addEventListener("touchend",(e) => {e.target.classList.remove("clickEffect")});
 
 let equalToButton = document.querySelector(".equalTo");
 equalToButton.addEventListener("click",equalToHandler);
@@ -73,9 +95,22 @@ let signButton = document.querySelector(".signButton");
 signButton.addEventListener("click",signButtonHandler);
 function signButtonHandler(e)
 {
-    if (signCheck === false)
+    if (signCheck === false && operatorCheck === false)
     {
-        textBox.value += '-';
+        textBox.value = '-' + textBox.value; 
+        signCheck = true;
+    }
+    if (signCheck === false && operatorCheck === true)
+    {
+        let stuffbeforeOperator = textBox.value.slice(0,indexOfOperator + 1);
+        if (textBox.value.length - 1 > indexOfOperator)
+        {
+            textBox.value = stuffbeforeOperator + '-' + textBox.value.slice(indexOfOperator + 1);
+        }
+        else
+        {
+            textBox.value = stuffbeforeOperator + '-';
+        }
         signCheck = true;
     }
 }
@@ -88,15 +123,16 @@ function operatorHandler(e)
     {
         num1 = Number(textBox.value);
         let numLen = textBox.value.length;
-        textBox.value += e.target.textContent;
+        textBox.value +=  ' ' + e.target.textContent + ' ';
         operator = e.target.textContent;
         if (num1 < 0 && operator == '-')
         {
-            indexOfOperator = textBox.value.indexOf(operator) + numLen;
+            indexOfOperator = textBox.value.indexOf(operator) + numLen ;
+            indexOfOperator += 2;
         }
         else
         {
-            indexOfOperator = textBox.value.indexOf(operator);
+            indexOfOperator = textBox.value.indexOf(operator) + 1;
         }
         operatorCheck = true;
         decimalPointCheck = false;
@@ -131,9 +167,26 @@ function operatorHandler(e)
                 break;
         }
     }
+    if (isNaN(num2) === false)
+    {
         result();
-        decimalPointCheck = true;
-        signCheck = true;
+        if (textBox.value.includes('.') === false)
+        {
+            decimalPointCheck = false;
+        }
+        else
+        {
+            decimalPointCheck = true;
+        }
+        if (textBox.value.includes('-') === false)
+            {
+                signCheck = false;
+            }
+            else
+            {
+                signCheck = true;
+            }
+    }
     }
 }
 operatorKeys.forEach((item) => {item.addEventListener("click",operatorHandler)});
